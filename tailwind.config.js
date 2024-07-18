dc15a8653fc524b156bb6f6f33228ff19c61f3d7
amelidiazchavez19 <diazchavez1905@gmail.com>
2024-07-18 17:59:10 -0500

    modificación estilos (class) header: buscador/iniciar sesion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    
     "./src/**/*.{html,ts}",
     "./app/**/*.{html,ts}",
     "./node_modules/flowbite/**/*.js" // add this line
